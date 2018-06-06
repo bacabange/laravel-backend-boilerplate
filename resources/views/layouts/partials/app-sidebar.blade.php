39791ea847257d01ac594c66a28d45054e260401
Stiven <stivencastillo.90@gmail.com>
2018-06-05 22:27:57 -0500

    LB: implement localization module from Laravel (es and en)

--- resources/views/layouts/partials/app-sidebar.blade.php
+++ resources/views/layouts/partials/app-sidebar.blade.php
@@ -9,7 +9,7 @@
     </div>
     <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
     <ul class="list-unstyled">
-        <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+        <li class="active"><a href="{{ route('dashboard') }}"> <i class="icon-home"></i>Dashboard </a></li>
         <li><a href="tables.html"> <i class="icon-grid"></i>Tables </a></li>
         <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
         <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
